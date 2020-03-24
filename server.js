139fa61798078ffb45a28f8ce94e6c7bf46b46ac
Lucas Ferreira <lferreira@liveperson.com>
2020-03-24 22:15:16 +0100

    change server log

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen(3000, console.log("Server running at port 3000"));
+app.listen(3000, console.log("Server running at http://localhost:3000"));
